df2dd6385d150ceca35326e917a65116c5d968a0
Nycole Xavier <nycole.xavier@hotmail.com>
2021-11-23 13:53:40 -0400

    lesson: primeiro componente React

---
+++ src/componentes/RepositoryList.jsx
@@ -0,0 +1,61 @@
+const repositoryName = 'unform3';
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list" >
+            <h1>Lista de repositório</h1>
+
+            <ul>
+                <li>
+                    <strong>(repositoryName)</strong>
+                    <p>forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+
+                </li>
+
+                <li>
+                    <strong>unform</strong>
+                    <p>forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+
+                </li>
+
+                <li>
+                    <strong>unform</strong>
+                    <p>forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+
+                </li>
+
+                <li>
+                    <strong>unform</strong>
+                    <p>forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+
+                </li>
+
+                <li>
+                    <strong>unform</strong>
+                    <p>forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+
+                </li>
+            </ul>
+        </section>
+    );
+}
