a8a448149cdd63366031b8dcd607c1ad699ee133
Nycole Xavier <nycole.xavier@hotmail.com>
2021-11-24 07:11:41 -0400

    lesson: Estado do componente finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { Counter } from './componentes/Counter';
+import { RepositoryItem } from './componentes/RepositoryItem';
 import { RepositoryList } from './componentes/RepositoryList';
 import './styles/global.scss';
 
 export function App(){
-    return <RepositoryList/>
+    return (
+        <>
+        <RepositoryList/>
+        <Counter/>
+        </>
+    )
 }
